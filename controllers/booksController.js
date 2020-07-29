6c2f2f299e034a004d104606fa52d34acceeb087
Branden Patten <patten.branden@gmail.com>
2020-07-29 04:36:03 -0400

    added comments to different fuctions explaining what they do

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -2,6 +2,7 @@ const db = require("../models");
 
 // Defining methods for the booksController
 module.exports = {
+  //returns all books in the inverse order of database submission
   findAll: function(req, res) {
     db.Book
       .find(req.query)
@@ -9,24 +10,28 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  //returns a specific book by it's ID value
   findById: function(req, res) {
     db.Book
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  //creates a book based on the book model
   create: function(req, res) {
     db.Book
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  //updates a book in the database by looking up it's ID value
   update: function(req, res) {
     db.Book
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  //removes a book from the database by ID value
   remove: function(req, res) {
     db.Book
       .findById({ _id: req.params.id })
